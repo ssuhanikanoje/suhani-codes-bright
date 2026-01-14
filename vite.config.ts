63554ab7d4dc283232b39b4c94ed3a80f10b1194
SUHANI SUDESH KANOJE <130144423+ssuhanikanoje@users.noreply.github.com>
2026-01-14 17:06:48 +0530

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/suhani-codes-bright/",   // 
   server: {
     host: "::",
     port: 8080,
@@ -16,3 +17,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
